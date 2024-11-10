99c1753779b29540bbcbff92bb269b895a4c7067
Marek Grzyb <marek@grzyb.dev>
2024-11-10 23:44:21 +0100

    Minor fixes

--- ueloctool/api/helpers.py
+++ ueloctool/api/helpers.py
@@ -5,7 +5,7 @@ def ReadString(buf: BufferedReader) -> str:
     length = int.from_bytes(buf.read(4), byteorder="little", signed=True)
 
     if length > 0:
-        result = buf.read(length).decode("utf-8")
+        result = buf.read(length).decode("ascii")
     elif length < 0:
         result = buf.read(length * -2).decode("utf-16")
     else:
